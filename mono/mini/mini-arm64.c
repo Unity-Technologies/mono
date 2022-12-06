779ab73db282f66b7ad495bcbcb6d3dae3460c82
Jem Patel <jem.patel@unity3d.com>
2022-12-06 19:12:31 +0000

    fix formatting and move code

--- mono/mini/mini-arm64.c
+++ mono/mini/mini-arm64.c
@@ -2084,6 +2084,8 @@ mono_arch_flush_icache (guint8 *code, gint size)
 
 	asm volatile ("dsb ish" : : : "memory");
 	asm volatile ("isb" : : : "memory");
+#elif HOST_WIN32	
+	FlushInstructionCache(code, NULL, size);
 #endif
 #endif
 }
