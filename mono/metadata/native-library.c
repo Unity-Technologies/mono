86b0c39aea228c3c85755b83d2192b9a44ee462f
marina-joffrineau <marina.joffrineau@unity3d.com>
2025-03-04 12:01:00 +0100

    edit warning message

--- mono/metadata/native-library.c
+++ mono/metadata/native-library.c
@@ -1206,18 +1206,9 @@ legacy_probe_for_module (MonoImage *image, const char *new_scope, char **load_er
 		const char* unity_new_scope = mono_get_find_plugin_callback () (new_scope, &hasError);
 		if (unity_new_scope == NULL || !unity_new_scope[0])
 		{
-			if (hasError == 1)
-			{
-				mono_trace (G_LOG_LEVEL_WARNING, MONO_TRACE_DLLIMPORT,
-					"DllImport unable to load unity mapped library '%s' : '%s'.",
-					unity_new_scope, load_error);
-			}
-			else
-			{
-				mono_trace (G_LOG_LEVEL_WARNING, MONO_TRACE_DLLIMPORT,
-					"DllImport unable to load unity mapped library '%s'",
-					unity_new_scope);
-			}
+			mono_trace (G_LOG_LEVEL_WARNING, MONO_TRACE_DLLIMPORT,
+				"DllImport unable to load unity mapped library '%s'",
+				unity_new_scope);
 		}
 
 		else
